0453eed93758a31561b3fb09431ff0b1271eabfe
Onur <jockey101@proton.me>
2026-02-22 14:24:51 -0500

    get box within window with text working w/o overfilling

--- src/libs.h
+++ src/libs.h
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <panel.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- src/main.c
+++ src/main.c
@@ -2,14 +2,40 @@
 #include <ncurses.h>
 #define TB_HEIGHT 3
 #define TB_WIDTH 14
+#define GENERATE_COUNT 100
+#define MAX_LINES 10
+#define LINE_BUF 512
 
-void drawTitleBox() {
-  int width, height;
-  getmaxyx(stdscr, height, width);
-  newwin(TB_HEIGHT, TB_WIDTH, height / 2, width / 2);
+static void render_input(WINDOW *win, char *buf, int *buf_len, int inner_h,
+                         int inner_w, int *out_row, int *out_col) {
+  werase(win);
+  box(win, 0, 0);
+
+  int row = 0, col = 0;
+  for (int i = 0; i < *buf_len; i++) {
+    if (row >= inner_h) {
+      *buf_len = i;
+    }
+    if (buf[i] == '\n') {
+      row++;
+      col = 0;
+    } else {
+      mvwaddch(win, 1 + row, 1 + col, (chtype)buf[i]);
+      col++;
+      if (col >= inner_w) {
+        row++;
+        col = 0;
+      }
+    }
+  }
 }
+// void drawTitleBox() {
+//   int width, height;
+//   getmaxyx(stdscr, height, width);
+//   newwin(TB_HEIGHT, TB_WIDTH, height / 2, width / 2);
 
 void drawOuter() { box(stdscr, 0, 0); }
+
 int main() {
   const char *words[] = {
       "this",     "is",       "a",       "text",     "execute",     "thing",
@@ -18,44 +44,69 @@ int main() {
       "options",  "words",    "list",    "program",  "programming", "move",
       "neovim",   "linux",    "fancy",   "navigate", "forget",      "new",
       "that",     "there",    "attempt", "lazy"};
-
-  initscr();
-  int width, height;
-  getmaxyx(stdscr, height, width);
-  cbreak();
-  noecho();
-  // nodelay(stdscr, TRUE);
-  keypad(stdscr, TRUE);
-  noqiflush();
-  srand(time(NULL));
-  drawOuter();
-  mvprintw(height - 2, 1, "press q to quit");
-  refresh();
-
   int word_count = sizeof(words) / sizeof(words[0]);
   int total_length = 0;
-  int generated_count = 20;
+  int generated_count = 100;
+  int width, height;
+  int ch;
+  char str[90];
+  initscr();
+  noecho();
+  keypad(stdscr, TRUE);
+
+  getmaxyx(stdscr, height, width);
+  srand(time(NULL));
+  drawOuter();
+  refresh();
+
   const char *word_dict[generated_count];
   for (int i = 0; i < generated_count; i++) {
     word_dict[i] = words[rand() % word_count];
   }
-  // word_dict[generated_count] = words[rand() % word_count];
 
-  move(6, 10);
-  int cWord = 0;
+  // padding, inner box pos
+  int pad_y = 2;
+  int pad_x = 4;
+  int box_h = height - pad_y * 2;
+  int box_w = width - pad_x * 2;
+  int box_y = pad_y;
+  int box_x = pad_x;
+  // inner box
+  WINDOW *wordwin = newwin(box_h, box_w, box_y, box_x);
+  box(wordwin, 0, 0);
+  wrefresh(wordwin);
+
+  int row = 1;
+  int col = 1;
   for (int n = 0; n < generated_count; n++) {
-    // printw("%s ", words[n]);
-    mvprintw(height / 2, width / 2, "%s ", word_dict[n]);
-    cWord++;
-    word_dict[cWord] = words[rand() % word_count];
+    const char *w = word_dict[n];
+    int len = strlen(w);
+    if (col + len >= box_w - 1) {
+      row++;
+      col = 1;
+    }
+    if (row >= box_h - 1) {
+      break;
+    }
+    mvwprintw(wordwin, row, col, "%s ", w);
+    col += len + 1;
   }
-  refresh();
+  wrefresh(wordwin);
+  // for (int n = 0; n < generated_count; n++) {
+  //   mvprintw(height / 2, width / 2, "%s ", word_dict[n]);
+  // }
+  // refresh();
 
-  int ch;
-  while ((ch = getch()) != 'q') {
-    mvprintw(height, 1, "pressed: %d  ", ch);
-    // refresh();
+  while ((ch = getch()) != 3) {
+    // mvprintw(1, 1, "pressed: %d  ", ch);
+    refresh();
   }
+  echo();
+  nocbreak();
+  refresh();
+  getstr(str);
+  // mvprintw(2, 0, "you entered: %s ", str);
+  // getch();
   // drawBox();
 
   endwin();
